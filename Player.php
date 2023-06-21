04890d88414470faaeb061b5e540344c63a85a1e
Khanhdev999 <135426117+Khanhdev999@users.noreply.github.com>
2023-06-21 18:01:08 +0700

    a
    a

---
+++ Player.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Player extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'birthday',
+        'phone',
+        'address',
+        'photo',
+    ];
+    public function team() {
+        return $this->belongsTo(Team::class);
+    }
+    public function coach(){
+        return $this->belongsTo(Coach::class);
+    }
+    public function tournaments(){
+        return $this->belongsToMany(Tournament::class);
+    }
+}
